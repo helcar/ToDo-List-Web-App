6df66b67e139add00dae3483303eb051cd98ddb7
unknown <qz1093@nyu.edu>
2020-08-07 11:09:15 -0400

    multipage with same template and same layout.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 const app = express();
 
 var newItems = ["the first item", "the second item", "the third item"];
+var newWorkItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -12,25 +13,37 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-  var todayDate = new Date();
-  var options = {
+  let todayDate = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = todayDate.toLocaleDateString("en-US", options);
+  let day = todayDate.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day, theNewItem: newItems});
+  res.render("list", {listOfTitle: day, theNewItem: newItems});
 })
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
-  newItems.push(item);
-  res.redirect("/");
-  //
+  // console.log(req.body);
+  let item = req.body.newItem;
+  if (req.body.list === "Work") {
+    newWorkItems.push(item);
+    res.redirect("/work");
+  } else {
+    newItems.push(item);
+    res.redirect("/")
+  }
 })
 
+app.get("/work", function(req, res){
+  res.render("list", {listOfTitle: "Work List", theNewItem: newWorkItems})
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
+})
 
 app.listen(port, function(){
   console.log("Hey there!");
